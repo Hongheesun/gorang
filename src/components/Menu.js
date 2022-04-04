5683a191576942abe6d894f3d5aed52ec16ecb6b
MiryangJung <miryang.jung@greenlabs.co.kr>
2022-04-04 20:38:56 +0900

    for  대신 map 사용

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,12 +2,6 @@ import React from "react";
 import styles from "../style/Menu.module.css";
 
 export default function Menu(props) {
-  const lis = [];
-  for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>{t.inputName}</li>);
-  }
-
   return (
     <>
       <div className="main">
@@ -20,7 +14,9 @@ export default function Menu(props) {
             추가
           </button>
           <ul>
-            {lis}
+            {
+              props.topics.map(item=><li key={item.id}>{item.inputName}</li>)
+            }
             {/* {content} */}
           </ul>
         </nav>
